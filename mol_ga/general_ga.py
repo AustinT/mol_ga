b86810c893c26b38c071573c24ff162d670c982d
Austin Tripp <ajt212@cam.ac.uk>
2023-07-08 22:44:28 +0100

    Remove a lot of unused arguments from general GA (simplify logic).

--- mol_ga/general_ga.py
+++ mol_ga/general_ga.py
@@ -6,18 +6,11 @@ import heapq
 
 import numpy as np
 
-from .cached_function import CachedBatchFunction, CachedFunction
+from .cached_function import CachedBatchFunction
 
 
 # Logger with standard handler
-ga_logger = logging.getLogger("genetic-algorithm")
-if len(ga_logger.handlers) == 0:
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    ch.setFormatter(formatter)
-    ga_logger.addHandler(ch)
+ga_logger = logging.getLogger(__name__)
 
 
 def run_ga_maximization(
@@ -30,12 +23,6 @@ def run_ga_maximization(
     population_size: int,
     offspring_size: int,
     num_population_samples_per_generation: Optional[int] = None,
-    patience: int = None,
-    max_func_calls: int = None,
-    y_transform: callable = None,  # only used if scoring function is not a cached function already
-    logger: logging.Logger = None,
-    smiles_filter: Union[callable, CachedFunction] = None,
-    early_stop_func: callable = None,
 ):
     """
     Runs a genetic algorithm to MAXIMIZE a score function.
@@ -51,45 +38,24 @@ def run_ga_maximization(
     # ============================================================
     # 0: Process input variables
     # ============================================================
-    if logger is None:
-        logger = ga_logger
+    logger = ga_logger
     logger.info("Starting GA maximization...")
     num_population_samples_per_generation = num_population_samples_per_generation or offspring_size
 
     # Create the cached function
     if not isinstance(scoring_function, CachedBatchFunction):
-        scoring_function = CachedBatchFunction(scoring_function, transform=y_transform)
+        scoring_function = CachedBatchFunction(scoring_function, )
     start_cache = dict(scoring_function.cache)
     start_cache_size = len(start_cache)
     logger.debug(f"Starting cache made, has size {start_cache_size}")
 
-    # Create max func calls
-    if max_func_calls is None:
-
-        # The most it could possibly be
-        max_func_calls = max_generations * offspring_size + len(
-            starting_population_smiles
-        )
-        max_func_calls *= 1000  # just to be safe
-    max_func_calls += start_cache_size  # since it will just be measured by cache size
-
-    # SMILES filter (cached in case it is expensive)
-    if smiles_filter is None or isinstance(smiles_filter, CachedFunction):
-        pass  # no need to do anything
-    else:
-        smiles_filter = CachedFunction(smiles_filter)  # cache it
-    if isinstance(smiles_filter, CachedFunction):
-        smiles_filter = smiles_filter.eval_non_batch  # so I can use "filter" function
 
     # ============================================================
     # 1: prepare initial population
     # ============================================================
 
     # Ensure unique
-    population_smiles = set(starting_population_smiles)
-
-    # Ensure passes filters
-    population_smiles = list(filter(smiles_filter, population_smiles))
+    population_smiles = list(set(starting_population_smiles))
 
     # Eval scores
     num_start_eval = len(set(population_smiles) - set(start_cache.keys()))
@@ -101,7 +67,7 @@ def run_ga_maximization(
     )
     del num_start_eval  # not needed later
     population_scores = scoring_function.eval_batch(population_smiles)
-    _curr_max_score = _starting_max_score = max(population_scores)
+    _starting_max_score = max(population_scores)
     logger.debug(
         f"Initial population scoring done. Pop size={len(population_smiles)}, Max={_starting_max_score}"
     )
@@ -116,14 +82,8 @@ def run_ga_maximization(
     # ============================================================
 
     # Run GA
-    early_stop = False
-    num_no_change_gen = 0
     gen_info = []
     for generation in range(max_generations):
-
-        # Skip everything if early stopping
-        if early_stop:
-            continue
 
         logger.info(f"Start generation {generation}")
 
@@ -142,11 +102,8 @@ def run_ga_maximization(
         logger.debug(f"\t{len(offspring)} created")
 
         # Add to population
-        population_smiles = set(population_smiles) | offspring
+        population_smiles = list(set(population_smiles) | offspring)
         logger.debug(f"\tNew population size = {len(population_smiles)}")
-
-        # Remove ones which don't pass filters
-        population_smiles = list(filter(smiles_filter, population_smiles))
 
         # Find out scores
         logger.debug("\tStarting function calls...")
@@ -179,31 +136,7 @@ def run_ga_maximization(
         logger.info(stats_str)
         gen_info.append(dict(smiles=population_smiles, **gen_stats_dict))
 
-        # early stopping if population doesn't change
-        if population_smiles == old_population_smiles:
-
-            num_no_change_gen += 1
-            logger.info(f"\tPopulation unchanged for {num_no_change_gen} generations")
-            if patience is not None and num_no_change_gen > patience:
-                logger.info(f"\tThis exceeds patience of {patience}. Terminating GA.")
-                early_stop = True
-                break
-        else:
-            num_no_change_gen = 0
-
-        # early stopping if budget is reached
-        if max_func_calls is not None and len(scoring_function.cache) >= max_func_calls:
-            logger.info(
-                f"\tBudget of {max_func_calls - start_cache_size} has been reached. Terminating..."
-            )
-            early_stop = True
-
-        # Early stopping from custom function
-        if early_stop_func is not None and early_stop_func(population):
-            early_stop = True
-
         # Clear variables from this iteration
-        _curr_max_score = max(population_scores)
         del population_scores, population_smiles
 
     # Return values
@@ -213,6 +146,5 @@ def run_ga_maximization(
         scoring_function.cache,  # holds all known function values
         (
             gen_info,
-            early_stop,
         ),  # GA logs
     )
