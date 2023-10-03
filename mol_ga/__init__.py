c588329e9ed404ddb692bb9b7802680bc707833a
Austin Tripp <ajt212@cam.ac.uk>
2023-10-03 12:31:24 +0100

    Make import work for python<3.8.
    Previously it would fail because importlib.metadata was not added until python 3.8.
    I put this into a try/except clause so it no longer fails.

--- mol_ga/__init__.py
+++ mol_ga/__init__.py
@@ -1,12 +1,15 @@
-from importlib.metadata import PackageNotFoundError, version
-
 from .general_ga import run_ga_maximization
 from .preconfigured_gas import default_ga
 
 __all__ = ["run_ga_maximization", "default_ga"]
 
 try:
-    __version__ = version("mol_ga")
-except PackageNotFoundError:
-    # package is not installed
-    pass
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        __version__ = version("mol_ga")
+    except PackageNotFoundError:
+        # package is not installed
+        pass
+except ModuleNotFoundError:
+    pass  # Python < 3.8
