95c0fdd59df73eb67785009e57e9de088750d107
Austin T <austin.james.tripp[at]gmail.com>
2024-04-22 19:57:05 -0700

    Changes to quantile sampling.
    1) Fix obvious typo in word "quantile"
    
    2) Add checks for empty population and NaNs

--- mol_ga/sample_population.py
+++ mol_ga/sample_population.py
@@ -6,7 +6,7 @@ from random import Random
 import numpy as np
 
 
-def uniform_qualitle_sampling(
+def uniform_quantile_sampling(
     population: list[tuple[float, str]],
     n_sample: int,
     rng: Random,
@@ -14,13 +14,19 @@ def uniform_qualitle_sampling(
 ) -> list[str]:
     """Sample SMILES by sampling uniformly from logarithmically spaced top-N."""
 
+    # Error handling
+    if len(population) == 0:
+        raise ValueError("Population is empty.")
+    if any(math.isnan(score) for score, _ in population):
+        raise ValueError("Population contains NaN scores (so quantiles cannot be computed).")
+
     samples: list[str] = []
     quantiles = 1 - np.logspace(-3, 0, 25)
-    n_samples_per_quanitile = int(math.ceil(n_sample / len(quantiles)))
+    n_samples_per_quantile = int(math.ceil(n_sample / len(quantiles)))
     for q in quantiles:
         score_threshold = np.quantile([s for s, _ in population], q)
         eligible_population = [smiles for score, smiles in population if score >= score_threshold]
-        samples.extend(rng.choices(population=eligible_population, k=n_samples_per_quanitile))
+        samples.extend(rng.choices(population=eligible_population, k=n_samples_per_quantile))
 
     # Shuffle samples to decrease correlations between adjacent samples
     if shuffle:
